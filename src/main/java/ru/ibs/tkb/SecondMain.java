a45dabcec95ff02212f71380988181431684de64
timurio <timos.gooogle@gmail.com>
2021-10-29 14:02:01 +0500

    Added SecondMain.java

---
+++ src/main/java/ru/ibs/tkb/SecondMain.java
@@ -0,0 +1,20 @@
+package ru.ibs.tkb;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SecondMain {
+    public static void main(String[] args) {
+
+        String command = "";
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+            command = bufferedReader.readLine();
+            System.out.println(StringUtils.lowerCase(command));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
