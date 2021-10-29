1f3839ab46d67a56ddf51f93e807a800bba98b8a
timurio <timos.gooogle@gmail.com>
2021-10-29 13:53:09 +0500

    String Output Implementation

--- src/main/java/ru/ibs/tkb/Main.java
+++ src/main/java/ru/ibs/tkb/Main.java
@@ -1,7 +1,18 @@
 package ru.ibs.tkb;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
 
+        String command = "";
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+            command = bufferedReader.readLine();
+            System.out.println(command);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
